c840530bc5c239422e4f5fde130ccb31f0d5b354
chick <chick@eecs.berkeley.edu>
2014-04-15 13:20:18 -0700

    Added a couple of icons clicking search icon will advance current search to next matching

--- ast_viewer/views/search_widget.py
+++ ast_viewer/views/search_widget.py
@@ -7,66 +7,67 @@ class SearchLineEdit(QtGui.QLineEdit):
     def __init__(self, parent=None, on_changed=None, on_next=None):
         QtGui.QLineEdit.__init__(self, parent)
 
-        self.clearButton = QtGui.QToolButton(self)
-        self.clearButton.setIcon(
+        self.clear_button = QtGui.QToolButton(self)
+        self.clear_button.setIcon(
             QtGui.QIcon.fromTheme(
                 "system-close",
                 fallback=QtGui.QIcon(QtGui.QPixmap(r"images/close_icon.png"))
             )
         )
-        #self.clearButton.setIconSize(clear_pixmap.size())
-        self.clearButton.setCursor(QtCore.Qt.ArrowCursor)
-        self.clearButton.setStyleSheet("QToolButton { border: none; padding: 0px;}")
-        self.clearButton.hide()
-        self.clearButton.clicked.connect(self.clear)
+        #self.clear_button.setIconSize(clear_pixmap.size())
+        self.clear_button.setCursor(QtCore.Qt.ArrowCursor)
+        self.clear_button.setStyleSheet("QToolButton { border: none; padding: 0px;}")
+        self.clear_button.hide()
+        self.clear_button.clicked.connect(self.clear)
         self.textChanged.connect(self.updateCloseButton)
         if on_changed:
             self.textChanged.connect(on_changed)
 
-        self.searchButton = QtGui.QToolButton(self)
-        self.searchButton.setIcon(
+        self.search_button = QtGui.QToolButton(self)
+        self.search_button.setIcon(
             QtGui.QIcon.fromTheme(
                 "system-search",
                 fallback=QtGui.QIcon(QtGui.QPixmap(r"images/search_icon.png"))
             )
         )
-#        self.searchButton.setIcon(QtGui.QIcon(search_pixmap))
-        #self.searchButton.setIconSize(search_pixmap.size())
-        self.searchButton.setStyleSheet("QToolButton { border: none; padding: 0px;}")
-        if on_next:
-            self.searchButton.clicked.connect(on_next)
+        self.search_button.setToolTip("Clicking this will advance search to next item")
+        self.search_button.setStyleSheet("QToolButton { border: none; padding: 0px;}")
 
-        frameWidth = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
+        if on_next:
+            self.search_button.clicked.connect(on_next)
+            self.returnPressed.connect(on_next)
+
+        frame_width = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
         self.setStyleSheet(
             "QLineEdit { padding-left: %spx; padding - right: % spx;} " %
-            (self.searchButton.sizeHint().width() + frameWidth + 1, self.clearButton.sizeHint().width() + frameWidth + 1)
+            (self.search_button.sizeHint().width() + frame_width + 1, self.clear_button.sizeHint().width() + frame_width + 1)
         )
         msz = self.minimumSizeHint()
         self.setMinimumSize(max(msz.width(),
-        self.searchButton.sizeHint().width() +
-        self.clearButton.sizeHint().width() + frameWidth * 2 + 2),
+        self.search_button.sizeHint().width() +
+        self.clear_button.sizeHint().width() + frame_width * 2 + 2),
         max(msz.height(),
-        self.clearButton.sizeHint().height() + frameWidth * 2 + 2))
+        self.clear_button.sizeHint().height() + frame_width * 2 + 2))
 
-    #        self.searchMenu = QtGui.QMenu(self.searchButton)
-    #        self.searchButton.setMenu(self.searchMenu)
+    #        self.searchMenu = QtGui.QMenu(self.search_button)
+    #        self.search_button.setMenu(self.searchMenu)
     #        self.searchMenu.addAction("Google")
-    #        self.searchButton.setPopupMode(QtGui.QToolButton.InstantPopup)
+    #        self.search_button.setPopupMode(QtGui.QToolButton.InstantPopup)
 
     def resizeEvent(self, event):
-        sz = self.clearButton.sizeHint()
+        sz = self.clear_button.sizeHint()
         frameWidth = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
-        self.clearButton.move(self.rect().right() - frameWidth -
+        self.clear_button.move(self.rect().right() - frameWidth -
                           sz.width(),
                           (self.rect().bottom() + 1 - sz.height()) / 2)
-        self.searchButton.move(self.rect().left() + 1,
+        self.search_button.move(self.rect().left() + 1,
                            (self.rect().bottom() + 1 - sz.height()) / 2)
 
     def updateCloseButton(self, text):
         if text:
-            self.clearButton.setVisible(True)
+            self.clear_button.setVisible(True)
         else:
-            self.clearButton.setVisible(False)
+            self.clear_button.setVisible(False)
 
 
 if __name__ == '__main__':
